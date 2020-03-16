36b1fca5af1826fac48ae9c34009391c8c16654e
Nicola Cimmino <nicola.cimmino@gmail.com>
2020-03-16 20:23:13 +0100

    Fix url, add screenshot

--- messages.h
+++ messages.h
@@ -7,7 +7,7 @@ const char message0[] PROGMEM =
     "  \\ \\  / /  \\ | |  | | |    | |     \e[32mBlock Mode:\e[33m CTR\r\n"
     "   \\ \\/ / /\\ \\| |  | | |    | |     \e[32mDev:\e[33m Nicola Cimmino\r\n"
     "    \\  / ____ \\ |__| | |____| |     \e[32mBuilt:\e[33m " __DATE__ " " __TIME__ "\r\n"
-    "     \\/_/    \\_\\____/|______|_|     \e[32mSrc:\e[33m https://github.com/nicolacimmino/pgen\r\n"
+    "     \\/_/    \\_\\____/|______|_|     \e[32mSrc:\e[33m github.com/nicolacimmino/vault\r\n"
     "                \e[32m(c) Nicola 2020";
 
 const char *const messages[] PROGMEM = {message0};
